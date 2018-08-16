34b0a38144cf407a36e986739e695adbc6e77201
Alexander Ela <ela.alexander@gmail.com>
2018-08-16 11:10:06 -0600

    Create is a  function test

---
+++ test/fruit-test.js
@@ -0,0 +1,19 @@
+const { assert } = require('chai');
+const Fruit = require('../lib/Fruit');
+
+describe('Fruit', () => {
+
+	it('should be a function', () => {
+		assert.isFunction(Fruit);
+	});
+
+	it('should have a default height and a width', () => {
+		var fruit new Fruit();
+		assert.equal();
+		assert.equal();
+	});
+
+
+
+
+});
